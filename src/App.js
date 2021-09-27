67b295b2a9cd746c1be1f46fbc1cea2116c400e1
Vasily93 <markovavasily@gmail.com>
2021-09-26 21:20:26 -0700

    final comment

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import PageContent from "./PageContent";
 import { ThemeProvider } from "./contexts/ThemeContext";
 import { LanguageProvider } from "./contexts/LanguageContext";
 
+//This is a way to pass contexts to react class components without react hooks!
+
 class App extends Component {
   render() {
     return (
